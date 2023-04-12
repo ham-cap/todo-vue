06e5a1c8f60c682467bd5d63756bcd76f04758b7
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-04-12 16:41:54 +0900

    Vueのプロジェクトを作成し不要なファイル等を削除・新規登録機能を仮実装

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodo === "") return;
+      this.todos.push(this.newTodo);
+      localStorage.setItem("todoList", JSON.stringify(this.todos));
+      this.newTodo = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>ToDo</h1>
+    <p>{{ $data }}</p>
+    <input v-model="newTodo" type="text" />
+    <button @click="addTodo">Add</button>
+  </div>
+</template>
