164682f27627f90a4adcd2cfb5475d0702dd584e
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-04-17 21:09:19 +0900

    編集機能の実装・削除機能の修正
    ・削除処理において、本来はdeleteキーが押された要素のみが取り除かれた状態のtodosがlocalStorageに保存されなければならないところ、削除対象以外の全タスクが削除された状態で保存されてしまっていため修正した。

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script>
 import CreateForm from "./components/CreateForm.vue";
+import EditForm from "./components/EditForm.vue";
 
 export default {
   components: {
     CreateForm,
+    EditForm,
   },
   data() {
     return {
       newTodo: "",
       todos: [],
+      editing: false,
+      selectedTodo: "",
+      selectedIndex: "",
+      updatedTodo: "",
     };
   },
   methods: {
@@ -21,8 +27,26 @@ export default {
       this.$refs.createForm.clearInput();
     },
     deleteTodo: function (index) {
-      this.todos.splice(index, 1);
+      this.todos.splice(index, 1)
+      localStorage.setItem("todoList", JSON.stringify(this.todos));
     },
+    editTodo: function (index) {
+      this.editing = true;
+      const currentTodosJson = localStorage.getItem('todoList');
+      const currentTodos = JSON.parse(currentTodosJson);
+      this.selectedTodo = currentTodos[index];
+      this.selectedIndex = index;
+    },
+    updateTodo: function (event, index) {
+      this.updatedTodo = event;
+      if (this.updatedTodo === "") return;
+      this.todos.splice(index, 1, this.updatedTodo);
+      localStorage.setItem("todoList", JSON.stringify(this.todos));
+      this.$refs.editForm.clearChangedTodo();
+      this.editing = false;
+      this.selectedTodo = "";
+      this.selectedIndex = "";
+    }
   },
   mounted() {
     if (Object.prototype.hasOwnProperty.call(localStorage, "todoList")) {
@@ -39,7 +63,8 @@ export default {
   <div>
     <h1>ToDo</h1>
     <p>Appのデータ{{ $data }}</p>
-    <CreateForm ref="createForm" v-on:addTodo="addTodo" />
+    <CreateForm ref="createForm" v-on:addTodo="addTodo" v-if="this.editing === false" />
+    <EditForm ref="editForm" v-on:updateTodo="updateTodo" v-bind:selectedTodo="selectedTodo" v-bind:selectedIndex="selectedIndex" v-else />
     <ul>
       <li v-for="(todo, index) in todos" v-bind:key="index">
         {{ index }}：{{ todo }}
---
+++ src/components/EditForm.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      changedTodo: this.selectedTodo,
+    };
+  },
+  methods: {
+    clearChangedTodo: function() {
+      this.changedTodo = "";
+    }
+  },
+  props: ["selectedTodo", "selectedIndex"]
+};
+</script>
+
+<template>
+  <div>
+    <input v-model="changedTodo" type="text" />
+    <button @click="$emit('updateTodo', this.changedTodo, this.selectedIndex)">Update</button>
+    <p>selectedTodo: {{ selectedTodo }}</p>
+    <p>selectedIndex: {{ selectedIndex }}</p>
+    <p>EditFormのデータ: {{ $data }}</p>
+  </div>
+</template>
